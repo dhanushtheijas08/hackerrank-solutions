a808b514561cb10869b08b17e9cc6fd901b0134e
dhanush_theijas <21cs033@kpriet.ac.in>
2023-02-08 22:24:10 +0530

    new file:   003.Arithmetic Operators/003.Answer.py 	new file:   003.Arithmetic Operators/003.Output.png 	new file:   003.Arithmetic Operators/003.Question.png 	new file:   004.Python Division/004.Answer.py 	new file:   004.Python Division/004.Output.png 	new file:   004.Python Division/004.Question.png

---
+++ 004.Python Division/004.Answer.py
@@ -0,0 +1,8 @@
+# Hacker Rank URL - https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=false
+
+# Answer :
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
